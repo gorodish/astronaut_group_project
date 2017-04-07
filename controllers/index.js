881a41ee9336448997af3cacde24ac6ae9ae340c
John Daly <jpdaly@gmail.com>
2017-04-07 12:50:30 +0100

    add hmtl pages for four modules, add models folder

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var path = require('path');
-var router = express.Router();
+var router = express.Router
+var client = path.join(__dirname + '../client/');
 
-router.get('/', path.join(__dirname + '../client/index.html'));
+router.get('/', client + 'index.html');
+
+router.get('/events', client + 'events.html');
