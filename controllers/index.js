32c87ea28293aa803424ff5e8a9c2801055a8ea2
John Daly <jpdaly@gmail.com>
2017-04-07 13:14:25 +0100

    routes created to temp html pages

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var path = require('path');
-var router = express.Router
-var client = path.join(__dirname + '../client/');
+var router = express.Router();
+var build = path.join(__dirname + '/../client/build/');
 
-router.get('/', client + 'index.html');
+router.use('/diary', require('./diaryController'));
 
-router.get('/events', client + 'events.html');
+router.get('/', function(req, res){
+  res.sendFile(build + 'index.html');
+});
+
+router.get('/events/', function(req, res){
+  res.sendFile(build + 'pages/events.html');
+});
+
+router.get('/map/', function(req, res){
+  res.sendFile(build + 'pages/events.html');
+});
+
+router.get('/youtube/', function(req, res){
+  res.sendFile(build + 'pages/events.html');
+});
+
+module.exports = router;
