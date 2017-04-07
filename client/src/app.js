4926733eed32f6c75677707a94920ae7cc63b6d7
John Daly <jpdaly@gmail.com>
2017-04-07 22:53:59 +0100

    diary list populates and auto-updats on adding new entry

--- client/src/app.js
+++ client/src/app.js
@@ -8,16 +8,61 @@ var makePostRequest = function(url, payload, callback){
   xhr.send(JSON.stringify(payload));
 }
 
+var getAllEntries = function(url, callback){
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.onload = callback;
+  xhr.send();
+}
+
+var getLastXEntries = function(){
+  var all = JSON.parse(this.responseText);
+  var last = all.length - 1
+  last4 = all.slice(last - 3, last + 1).reverse();
+
+  renderEntries(last4);
+}
+
+var renderEntries = function(array){
+  var recent = document.querySelector('#recent-entries');
+  recent.innerText = "";
+
+  array.forEach(function(entry){
+    var text = document.createElement('li');
+    text.innerText = entry.text;
+    var date = document.createElement('li');
+    date.innerText = new Date(entry.timeSinceEpoch);
+    var where = document.createElement('li');
+    where.innerText = 'far away';
+    recent.appendChild(text);
+    recent.appendChild(date);
+    recent.appendChild(where);
+  });
+}
+
+var updateRecent = function(){
+  getAllEntries('/api/diary', getLastXEntries);
+  
+}
+
 
 var app = function(){
   var diarySubmit = document.querySelector('#diary-submit');
   var diaryInput = document.querySelector('#diary-input');
+
+  var seeAll = document.querySelector('#see-all-entries');
+  
   diarySubmit.onclick = function(){
     var entry = new Diary(diaryInput.value);
     makePostRequest('/api/diary', entry, function(){
       console.log(JSON.parse(this.responseText));
     })
+    updateRecent()
   }
+
+  seeAll
+updateRecent();
+
 }
 
 window.onload = app;
