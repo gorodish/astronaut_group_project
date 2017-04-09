d661ae03920d5e6c53608379046fab83ca2fd493
Daniel McDonald <danielkg91@gmail.comssh-keygen -t rsa -C danielkg91@gmail.com>
2017-04-09 12:15:26 +0100

    added html elements and added click listener and xhr request

--- client/src/app.js
+++ client/src/app.js
@@ -1,107 +1,26 @@
-var Diary = require('./models/DiaryConstructor');
-
-var makePostRequest = function(url, payload, callback){
+var makeRequest= function(url, callback){
   var xhr = new XMLHttpRequest();
-  xhr.open('POST', url);
-  xhr.setRequestHeader('content-type', 'application/json');
   xhr.onload = callback;
-  xhr.send(JSON.stringify(payload));
-}
-
-var getAllEntries = function(url, callback){
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url);
-  xhr.onload = callback;
-  xhr.send();
-}
-
-var getLastXEntries = function(){
-  var all = JSON.parse(this.responseText);
-  var last = all.length - 1
-  last4 = all.slice(last - 3, last + 1).reverse();
-
-  populateRecent(last4);
-}
-
-
-var renderList = function(array, element){
-  array.forEach(function(entry){
-   var text = document.createElement('li');
-   var diaryText = entry.text;
-   diaryText.length > 140 ? diaryText = diaryText.substring(0,139)+"..." : null;
-   text.innerText = diaryText;
-   element.appendChild(text);
-
-   var childList = document.createElement('ul');
-   text.appendChild(childList);
-
-   var date = document.createElement('li');
-   date.innerText = new Date(entry.timeSinceEpoch);
-   childList.appendChild(date);
-
-   var where = document.createElement('li');
-   where.innerText = 'far away';
-   childList.appendChild(where);
- })
-}
-
-var populateRecent = function(array){
-  var recent = document.querySelector('#recent-entries');
-  recent.innerText = "";
-
-  renderList(array, recent);
-
-  
-};
-
-
-var updateRecent = function(){
-  getAllEntries('/api/diary', getLastXEntries);
-  
-}
-
-var showAll = function(){
-  var seeAllView = document.querySelector('#see-all-view');
-  seeAllView.style.display = "flex";
-  seeAllView.innerText = "";
-  var ul = document.createElement('ul');
-  seeAllView.appendChild(ul);
-  var all = JSON.parse(this.responseText).reverse();
-
-  renderList(all, ul);
-
+  xhr.open("GET", url);
+  xhr.send(); 
 }
 
 var app = function(){
-  var diarySubmit = document.querySelector('#diary-submit');
-  var diaryInput = document.querySelector('#diary-input');
+  var btn = document.querySelector("#SubmitBtn");
+  var input = document.querySelector("#SearchField");
+  btn.onclick = function(){
+  var youTubeUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyD-DrQFCTZv7slK1SPSJa3QQCrgDHIC3q8&q=' + input.value;
 
-  var seeAll = document.querySelector('#toggle-view');
+makeRequest(youTubeUrl, function(){
+  var details = JSON.parse(this.responseText);
+  console.log(details.items[0].snippet.title);
+  })
+
+
+  }
   
-  diarySubmit.onclick = function(){
-    if(!diaryInput.value){
-      alert('You need to type something!');
-      return;
-    }
-    var entry = new Diary(diaryInput.value);
-    diaryInput.value = "";  
-    makePostRequest('/api/diary', entry, function(){
-      console.log(JSON.parse(this.responseText));
-    })
-    updateRecent();
-  }
 
-  seeAll.onclick = function(){
-    var newEntryView = document.querySelector('#new-entry-view');
-    newEntryView.style.display = "none";
-    seeAll.innerText = 'new entry';
-
-    getAllEntries('/api/diary', showAll);
-
-
-  }
-
-  updateRecent();
+  
 
 }
 
