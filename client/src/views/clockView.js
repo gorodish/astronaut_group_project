29b50011acf6ab728be5e7a84556db306598145b
Ryan Sinclair <sinky1903@aol.com>
2017-04-11 14:03:07 +0100

    countdown timer has now been added and working. some styling has been applied to overall dash and local quadrants also

--- client/src/views/clockView.js
+++ client/src/views/clockView.js
@@ -1,7 +1,5 @@
 
 
-
-
 var clockView = function(){
   var headingClock = document.querySelector('#heading-clock');
   var ul = document.createElement('ul');
@@ -27,7 +25,58 @@ var clockView = function(){
     utcTime.innerText = 'UTC: ' + epochTime.toLocaleTimeString('UTC', {timeZone: 'UTC'});
     moskovskoeVremya.innerText = 'Москва: ' + epochTime.toLocaleTimeString('ru-RU', {timeZone: 'Europe/Moscow'});
     houstonTime.innerText = 'Houston: ' + epochTime.toLocaleTimeString('en-US', {timeZone: 'America/Chicago'});
-  }, 1000)
+  }, 1000);
+
+////////////////////////////////////////////////
+
+  var deadline = 'April 23 2017 23:59:59 GMT+0200';
+
+  function getTimeRemaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 ); 
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {
+      'total': t,
+      days: days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds      
+    };
+  };
+
+  getTimeRemaining(deadline).minutes;
+
+  function initializeClock(id, endtime){
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock(){
+
+      var t = getTimeRemaining(endtime);
+
+      daysSpan.innerHTML = t.days;
+      hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+      minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+      secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+      if(t.total<=0){
+        clearInterval(timeInterval);
+
+
+      };
+    }
+
+    updateClock(); // run function once at first to avoid delay
+    var timeInterval = setInterval(updateClock,1000);
+  };
+
+  initializeClock('clockdiv', deadline);
+
 }
 
 module.exports = clockView;
